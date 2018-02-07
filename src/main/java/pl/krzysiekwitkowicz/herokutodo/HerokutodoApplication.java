c26eef5171bba32cba459766151debf9f415eb56
kwitkowicz <krzysiekwitkowicz@gmail.com>
2018-02-07 11:13:41 +0100

    Initial commit

---
+++ src/main/java/pl/krzysiekwitkowicz/herokutodo/HerokutodoApplication.java
@@ -0,0 +1,12 @@
+package pl.krzysiekwitkowicz.herokutodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokutodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokutodoApplication.class, args);
+	}
+}
